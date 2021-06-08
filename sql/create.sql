a939d5027db05cbe51be00b6acfcebdb75ef3167
fairay <48217178+fairay@users.noreply.github.com>
2021-06-08 22:33:56 +0300

    ADD: sql tables constraints

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,3 @@
-DROP TABLE Accounts CASCADE;
-
 CREATE TABLE IF NOT EXISTS Accounts(
 	Login 			TEXT 			PRIMARY KEY,
 	PersType 		TEXT 			NOT NULL,
@@ -7,30 +5,39 @@ CREATE TABLE IF NOT EXISTS Accounts(
 	HashedPassword	TEXT			NOT NULL
 );
 
-DROP TABLE Person CASCADE;
 CREATE TABLE IF NOT EXISTS Person(
 	Login			TEXT			REFERENCES Accounts(Login) NOT NULL PRIMARY KEY,
 	Surname 		VARCHAR(40) 	NOT NULL,
 	Forename 		VARCHAR(40)		NOT NULL,
 	DOB				DATE			NOT NULL,
 	Gender			CHAR			NOT NULL,
-	PhoneNumber 	TEXT
+	PhoneNumber 	VARCHAR(20)		NOT NULL
 );
+ALTER TABLE Person ADD CONSTRAINT check_gender
+CHECK (Gender='м' OR Gender='ж');
 
-DROP TABLE Delivery CASCADE;
+ALTER TABLE Person ADD CONSTRAINT check_age
+CHECK (DOB < NOW() - interval '16 years');
+
+
 CREATE TABLE IF NOT EXISTS Delivery (
 	OrderID 		SERIAL			PRIMARY KEY,
 	Login			TEXT			REFERENCES Person(Login),
 	Address 		TEXT 			NOT NULL,
-	PhoneNumber 	VARCHAR(40)		NOT NULL,
+	PhoneNumber 	VARCHAR(20)		NOT NULL,
 	CreationTime	TIMESTAMP		NOT NULL,
 	CompletionTime	TIMESTAMP,
 	Status			VARCHAR(20)		NOT NULL,
 	Description 	TEXT			NOT NULL
 );
-ALTER TABLE Delivery 
-ADD CONSTRAINT check_status
+ALTER TABLE Delivery ADD CONSTRAINT check_status
 CHECK (Status='delivered' OR Status='in_transit' OR Status='not_assigned');
+
+ALTER TABLE Delivery ADD CONSTRAINT check_completion_time
+CHECK ((CompletionTime IS NULL) OR (CreationTime < CompletionTime));
+
+ALTER TABLE Delivery ADD CONSTRAINT check_cretion_time
+CHECK (CreationTime < NOW() + interval '1 minute');
 
 
 CREATE TABLE IF NOT EXISTS Trucks (
@@ -41,7 +48,7 @@ CREATE TABLE IF NOT EXISTS Trucks (
 CREATE TABLE IF NOT EXISTS Checkpoints (
 	CheckpointID	SERIAL			PRIMARY KEY,
 	Address 		TEXT 			NOT NULL,
-	PhoneNumber 	VARCHAR(40)		NOT NULL
+	PhoneNumber 	VARCHAR(20)		NOT NULL
 );
 
 
@@ -53,29 +60,42 @@ CREATE TABLE IF NOT EXISTS PassRecords (
 	Direction		VARCHAR(3)		NOT NULL
 	CONSTRAINT direction_valid CHECK (Direction='in' OR Direction='out')
 );
+ALTER TABLE PassRecords ADD CONSTRAINT check_pass_time
+CHECK (PassTime < NOW() + interval '1 minute');
 
 
-CREATE TABLE IF NOT EXISTS GuardDutys (
-	DutyID 			SERIAL			PRIMARY KEY,
-	CheckpointID	INT				REFERENCES Checkpoints(CheckpointID) NOT NULL,
-	Login			TEXT			REFERENCES Person(Login) NOT NULL,
+CREATE TABLE IF NOT EXISTS DutyRules (
+	RuleID			SERIAL			PRIMARY KEY,
 	BeginDate		DATE			NOT NULL,
 	EndDate			DATE,
 	BeginTime		TIME			NOT NULL,
 	EndTime			TIME			NOT NULL,
 	DOW				VARCHAR(7)		NOT NULL
 );
+ALTER TABLE DutyRules ADD CONSTRAINT check_duty_dates
+CHECK (BeginDate <= EndDate);
 
-CREATE TABLE IF NOT EXISTS DriverDutys (
+ALTER TABLE DutyRules ADD CONSTRAINT check_duty_times
+CHECK (BeginTime < EndTime);
+
+ALTER TABLE DutyRules ADD CONSTRAINT check_dow
+CHECK (DOW SIMILAR TO '[0|1|2|3|4|5|6]{1,7}');
+
+
+CREATE TABLE IF NOT EXISTS GuardDuty (
+	DutyID 			SERIAL			PRIMARY KEY,
+	CheckpointID	INT				REFERENCES Checkpoints(CheckpointID) NOT NULL,
+	Login			TEXT			REFERENCES Person(Login) NOT NULL,
+	RuleID			INT				REFERENCES DutyRules(RuleID) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS DriverDuty (
 	DutyID 			SERIAL			PRIMARY KEY,
 	PlateNumber		TEXT			REFERENCES Trucks(PlateNumber) NOT NULL,
 	Login			TEXT			REFERENCES Person(Login) NOT NULL,
-	BeginDate		DATE			NOT NULL,
-	EndDate			DATE,
-	BeginTime		TIME			NOT NULL,
-	EndTime			TIME			NOT NULL,
-	DOW				VARCHAR(7)
+	RuleID			INT				REFERENCES DutyRules(RuleID) NOT NULL
 );
+
 
 CREATE TABLE IF NOT EXISTS LogActions (
 	Actor 		VARCHAR(15)			NOT NULL,
